5db6dcd57c94c48764c0954790087df64a4062b7
pablo1479 <130510225+pablo1479@users.noreply.github.com>
2024-04-23 11:01:11 -0700

    pushing working home page

---
+++ my-app/src/components/pages/homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the about page of our website.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
