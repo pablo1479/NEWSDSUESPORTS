b52449492cff438c10e47e0b0caa8eeb59b57185
pablo1479 <130510225+pablo1479@users.noreply.github.com>
2024-04-23 11:01:11 -0700

    pushing working home page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import homepage from './components/pages/homepage'; // Make sure to capitalize component names
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<homepage />} /> {/* Use `element` prop and self-closing tags */}
+          <Route path="/about" element={<p>About Page</p>} /> {/* Example for About */}
+          <Route path="/contact" element={<p>Contact Page</p>} /> {/* Example for Contact */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
