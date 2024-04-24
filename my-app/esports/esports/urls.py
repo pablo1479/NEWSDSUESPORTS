3bf87ac683a2d5e04a9a41aea90e7019890d36e8
Brandon Reynolds <brandonreynolds1227@gmail.com>
2024-04-23 23:41:45 -0700

    Fixing home

--- my-app/esports/esports/urls.py
+++ my-app/esports/esports/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from .views import RegisterAPIView, LoginAPIView, LogoutAPIView, MerchandiseListCreateAPIView, StreamListAPIView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/register/', RegisterAPIView.as_view(), name='register'),
+    path('api/login/', LoginAPIView.as_view(), name='login'),
+    path('api/logout/', LogoutAPIView.as_view(), name='logout'),
+    path('api/merchandise/', MerchandiseListCreateAPIView.as_view(), name='merchandise-list-create'),
+    path('api/streams/', StreamListAPIView.as_view(), name='stream-list'),
 ]
