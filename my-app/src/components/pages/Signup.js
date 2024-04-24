7a03410d071621a7a4660661d1c2ebc2c7359e09
pablo1479 <130510225+pablo1479@users.noreply.github.com>
2024-04-23 17:10:59 -0700

    Fixed login signup home page nav bar

--- my-app/src/components/pages/Signup.js
+++ my-app/src/components/pages/Signup.js
@@ -13,18 +13,6 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        SDSU Esports
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 // TODO remove, this demo shouldn't need to reset the theme.
 
@@ -126,7 +114,6 @@ export default function SignUp() {
             </Grid>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 5 }} />
       </Container>
     </ThemeProvider>
   );
