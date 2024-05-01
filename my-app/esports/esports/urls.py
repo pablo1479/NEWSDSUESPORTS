69b91f24c08ec3cac16207d28d057e0729f9b859
Alex Rivera <35387065+Alexriv@users.noreply.github.com>
2024-05-01 07:36:26 -0700

    Revert "Fixed the backend!!!"
    This reverts commit 6f4dc308fb35ccd2634d50db6a03364b70537db1.

---
+++ my-app/esports/esports/urls.py
@@ -0,0 +1,26 @@
+"""
+URL configuration for esports project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from my-app.views import RegisterAPIView, LoginAPIView, LogoutAPIView, MerchandiseListCreateAPIView, StreamListAPIView
+
+urlpatterns = [
+    path('api/register/', RegisterAPIView.as_view(), name='register'),
+    path('api/login/', LoginAPIView.as_view(), name='login'),
+    path('api/logout/', LogoutAPIView.as_view(), name='logout'),
+    path('api/merchandise/', MerchandiseListCreateAPIView.as_view(), name='merchandise-list-create'),
+    path('api/streams/', StreamListAPIView.as_view(), name='stream-list'),
+]
